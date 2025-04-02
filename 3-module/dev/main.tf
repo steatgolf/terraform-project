518e5197eaa1f19905f10faf35e881d0de1048ce
https://gitlab.com/steatgolf <sarawoot.golf@hotmail.com>
2025-04-02 15:15:19 +0700

    add module and backend s3

---
+++ 3-module/dev/main.tf
@@ -0,0 +1,51 @@
+terraform {
+
+  #############################################################
+  ## AFTER RUNNING TERRAFORM APPLY (WITH LOCAL BACKEND)
+  ## YOU WILL UNCOMMENT THIS CODE THEN RERUN TERRAFORM INIT
+  ## TO SWITCH FROM LOCAL BACKEND TO REMOTE AWS BACKEND
+  #############################################################
+
+  backend "s3" {
+    bucket                   = "dev-steat-sj-terraform-tfstate"
+    key                      = "dev/terraform.tfstate"
+    region                   = "us-east-1"
+    shared_credentials_files = ["~/.aws/credentials"]
+    profile                  = "terraform-sa"
+    use_lockfile             = true
+    encrypt                  = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.92.0"
+    }
+  }
+}
+
+#Configure the AWS Provider
+provider "aws" {
+  region                   = "us-east-1"
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "terraform-sa"
+}
+
+
+locals {
+  environment_name = "dev"
+}
+
+module "main" {
+  source = "../../1-main"
+
+  # Input Variables
+  s3_bucket_name = "${local.environment_name}-steat-sj-terraform-tfstate"
+  #   domain           = "devopsdeployed.com"
+  #   environment_name = local.environment_name
+  #   instance_type    = "t2.micro"
+  #   create_dns_zone  = false
+  #   db_name          = "${local.environment_name}mydb"
+  #   db_user          = "foo"
+  #   db_pass          = var.db_pass
+}
